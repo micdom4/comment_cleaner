cadb22174826c6af422bb5bb254d86b9d66b2dd7
micdom <domagalamichal04@gmail.com>
2026-03-06 01:21:26 +0100

    Initial plugin files with simple mechanic

---
+++ src/main/java/pl/zzpj/plugin/ReviewCommentsAction.java
@@ -0,0 +1,24 @@
+package pl.zzpj.plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class ReviewCommentsAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+
+        Messages.showInfoMessage(
+                project,
+                "IT`S ALIVE! IT`S ALIVE!",
+                "Simple Info Message"
+        );
+    }
+}
