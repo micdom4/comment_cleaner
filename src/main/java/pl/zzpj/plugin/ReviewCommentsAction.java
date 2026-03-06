48d4abb91f533373d2d57d7f48946fea52c23a13
micdom <domagalamichal04@gmail.com>
2026-03-06 01:31:43 +0100

    Added comments counter

--- src/main/java/pl/zzpj/plugin/ReviewCommentsAction.java
+++ src/main/java/pl/zzpj/plugin/ReviewCommentsAction.java
@@ -3,8 +3,13 @@ package pl.zzpj.plugin;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewCommentsAction extends AnAction {
 
@@ -15,10 +20,36 @@ public class ReviewCommentsAction extends AnAction {
             return;
         }
 
+        List<PsiComment> foundComments = new ArrayList<>();
+
+        PsiManager psiManager = PsiManager.getInstance(project);
+
+        ProjectFileIndex.getInstance(project).iterateContent(virtualFile -> {
+
+            if (!virtualFile.isDirectory() && !virtualFile.getFileType().isBinary()) {
+
+                PsiFile psiFile = psiManager.findFile(virtualFile);
+
+                if (psiFile != null) {
+                    psiFile.accept(new PsiRecursiveElementWalkingVisitor() {
+                        @Override
+                        public void visitElement(@NotNull PsiElement element) {
+                            if (element instanceof PsiComment) {
+                                foundComments.add((PsiComment) element);
+                            }
+                            super.visitElement(element);
+                        }
+                    });
+                }
+            }
+
+            return true;
+        });
+
         Messages.showInfoMessage(
                 project,
-                "IT`S ALIVE! IT`S ALIVE!",
-                "Simple Info Message"
+                "There are " + foundComments.size() + " comments in your project`s source files.",
+                "Comments Quantity"
         );
     }
 }
